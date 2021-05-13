e155aa7578b4d421cbab3b2c5e6bd761623384ae
komp <komp@ardinvest.net>
2021-05-13 16:35:39 +0300

    major edit

--- olt_backup.sh
+++ olt_backup.sh
@@ -21,6 +21,11 @@ OLTBackupDir="/srv/tftp/cfg/OLT/"
 # Number of parallel telnet connection
 NumProcs="5"
 
+# Check $1 parameter
+if [[ -z "$1" ]]; then
+	exit
+fi
+
 # Create log directory:
 if [[ ! -e "${OLTLogDir}" ]]; then
 	mkdir -p "${OLTLogDir}"
@@ -32,8 +37,8 @@ if [[ ! -e "${TftpLocalPath}""${TftpRemotePath}" ]]; then
 	chown -R tftp:tftp "${TftpLocalPath}"
 fi
 
-# Function for OLT models P3310C, P3608-2TE, P3616-2TE
-p3310c_p3608-2te_p3616-2te() {
+# Template function for file transfer from OLT
+bdcom_olt() {
 	(
 		sleep 2
 		echo "${OLTLogin}"
@@ -42,32 +47,7 @@ p3310c_p3608-2te_p3616-2te() {
 		sleep 1
 		echo "enable"
 		sleep 1
-		echo "copy startup-config tftp://${TftpRemotePath}startup-config_${OLTName} ${TftpIP}"
-		sleep 2
-		echo "copy flash:ifindex-config tftp://${TftpRemotePath}ifindex-config_${OLTName} ${TftpIP}"
-		sleep 2
-		echo "copy flash:config.db tftp://${TftpRemotePath}config.db_${OLTName} ${TftpIP}"
-		sleep 10
-		echo "quit"
-		sleep 1
-		echo "quit"
-	) | telnet ${OLTIPAddr} | tee ${OLTLogDir}$(date +%Y-%m-%d)_${OLTName}_${OLTIPAddr}.log &&
-	cd ${TftpLocalPath}${TftpRemotePath}
-	tar --force-local -czf ${OLTName}_${OLTIPAddr}.tar.gz startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName} &&
-	rm -f startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName}
-}
-
-# Function for OLT model P3608B
-p3608b() {
-	(
-		sleep 2
-		echo "${OLTLogin}"
-		sleep 1
-		echo "${OLTPassword}"
-		sleep 1
-		echo "enable"
-		sleep 1
-		echo "copy startup-config tftp://${TftpRemotePath}startup-config_${OLTName} ${TftpIP}"
+		echo "copy flash:startup-config tftp://${TftpRemotePath}startup-config_${OLTName} ${TftpIP}"
 		sleep 2
 		echo "copy flash:ifindex-config tftp://${TftpRemotePath}ifindex-config_${OLTName} ${TftpIP}"
 		sleep 2
@@ -80,8 +60,13 @@ p3608b() {
 		echo "quit"
 	) | telnet ${OLTIPAddr} | tee ${OLTLogDir}$(date +%Y-%m-%d)_${OLTName}_${OLTIPAddr}.log &&
 	cd ${TftpLocalPath}${TftpRemotePath}
-	tar --force-local -czf ${OLTName}_${OLTIPAddr}.tar.gz startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName} vos.conf_${OLTName} &&
-	rm -f startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName} vos.conf_${OLTName}
+	if [[ -f vos.conf_${OLTName} ]]; then
+		tar --force-local -czf ${OLTName}_${OLTIPAddr}.tar.gz startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName} vos.conf_${OLTName} &&
+		rm -f startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName} vos.conf_${OLTName}
+	else
+		tar --force-local -czf ${OLTName}_${OLTIPAddr}.tar.gz startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName} &&
+		rm -f startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName}
+	fi
 }
 
 # Main loop
@@ -89,10 +74,8 @@ for ((i = 0; i < NumProcs; i++)); do
 	(
 		while read -r OLTName OLTIPAddr OLTModel; do
 			PingResult=$(ping -c 3 -W 1 -A ${OLTIPAddr} | grep transmitted | awk '{ print $6 }')
-			if [[ ${PingResult} != "100%" && ${OLTModel} == "P3310C" || ${OLTModel} == "P3608-2TE" || ${OLTModel} == "P3616-2TE" ]]; then
-				p3310c_p3608-2te_p3616-2te
-			elif [[ ${PingResult} != "100%" && ${OLTModel} == "P3608B" ]]; then
-				p3608b
+			if [[ ${PingResult} != "100%" ]]; then
+				bdcom_olt
 			fi
 		done < <(awk -v NumProcs=${NumProcs} -v i="$i" \
 			'NR % NumProcs == i { print }' < ${Inventory})
