dd48816552778bfbe88b2f26a14a292419abff9c
komp <komp@ardinvest.net>
2024-05-31 17:41:43 +0300

    Update ping check. Log unreachable devices. Add archive logs. Update readme. Some fixes

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
 # BDCOM GEPON OLT configuration backup script
 
-Script must run on tftp server.
+Script must be run on tftp server. In my case tftp runs on Debian.
 
-It makes backups from multiple devices same time (tune "NumProcs" variable).
+It creates backups of several devices at the same time (tune `NumProcs` variable).
 
-* Supported models:
-```
-P3310C
-P3310D
-P3608B
-P3608-2TE
-P3616-2TE
-```
+* Supported all BDCOM GEPON OLT models.
 
-* Usage:
+## Usage
+
 ```
 ./olt_backup.sh olt_inventory.txt
 ```
-Inventory file must be like this:
+
+The inventory file should look like this:
 
 (name ip_address model)
 ```
@@ -27,9 +22,9 @@ OLT_Lenina_2 192.168.10.12 P3608B
 OLT_Tolstogo_15 192.168.10.13 P3616-2TE
 OLT_Gogolya_7 192.168.10.14 P3608B
 ```
-You will get archives on your tftp server:
+You will get the archives on your tftp server:
 ```
-ls -lh /srv/tftp/cfg/OLT/OLT_Pushkina_3/
+ls -lh /srv/tftp/OLT_backups/OLT_Pushkina_3/
 
 -rw-r--r-- 1 root root 26K Jul 11 05:32 2021-07-11_OLT_Pushkina_3_192.168.10.10.tar.gz
 -rw-r--r-- 1 root root 26K Jul 12 05:32 2021-07-12_OLT_Pushkina_3_192.168.10.10.tar.gz
@@ -48,8 +43,9 @@ running-config_OLT_Name
 
 * Cron job example:
 
+/etc/cron.d/olt_backup
 ```shell
-/etc/cron.d/olt_backup 
-
 0 5 * * * root /opt/backup/olt_backup.sh /opt/backup/olt_inventory.txt > /dev/null 2>&1
 ```
+
+Backup logs are stored in `/var/log/olt_backup_logs/` by default.
--- olt_backup.sh
+++ olt_backup.sh
@@ -11,13 +11,14 @@ TftpIP="192.168.20.20"
 OLTLogin="login"
 OLTPassword="password"
 # Directory variables
-TftpRemotePath="cfg/OLT/"
-TftpLocalPath="/srv/tftp/"
+TftpRemotePath="OLT_backups/"
 OLTLogDir="/var/log/olt_backup_logs/"
-OLTBackupDir="/srv/tftp/cfg/OLT/"
+OLTBackupDir="/srv/tftp/OLT_backups/"
 BackupDate="$(date +%Y-%m-%d)"
 # Number of backups to keep
 BackupCount="30"
+#How many days to keep logs?
+LogKeepDays="30"
 # Number of parallel telnet connection
 NumProcs="15"
 
@@ -29,7 +30,7 @@ fi
 # Create Backup directory:
 if [[ ! -e ${OLTBackupDir} ]]; then
   mkdir -p ${OLTBackupDir}
-  chown -R tftp:tftp ${TftpLocalPath}
+  chown -R tftp:tftp ${OLTBackupDir}
 fi
 
 # Create log directory:
@@ -37,8 +38,9 @@ if [[ ! -e ${OLTLogDir} ]]; then
   mkdir -p ${OLTLogDir}
 fi
 
-# Template function for file transfer from OLT
+# Function for sending files from OLT to tftp server.
 bdcom_olt() {
+  # OLT cli commands
   (
     sleep 2
     echo "${OLTLogin}"
@@ -56,9 +58,9 @@ bdcom_olt() {
     echo "copy flash:config.db tftp://${TftpRemotePath}${OLTName}/config.db_${OLTName} ${TftpIP}"
     sleep 10
     echo "show running-config | redirect running-config"
-    sleep 10
+    sleep 30
     echo "copy flash:running-config tftp://${TftpRemotePath}${OLTName}/running-config_${OLTName} ${TftpIP}"
-    sleep 2
+    sleep 5
     echo "delete running-config"
     sleep 1
     echo "y"
@@ -66,8 +68,10 @@ bdcom_olt() {
     echo "quit"
     sleep 1
     echo "quit"
+    sleep 1
   ) | telnet ${OLTIPAddr} | tee ${OLTLogDir}${OLTName}_${BackupDate}_${OLTIPAddr}.log
   cd ${OLTBackupDir}${OLTName}
+  # Create backup archive
   if [[ -f vos.conf_${OLTName} ]]; then
     tar --force-local -czf ${BackupDate}_${OLTName}_${OLTIPAddr}.tar.gz startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName} vos.conf_${OLTName} running-config_${OLTName} && \
     rm -f startup-config_${OLTName} ifindex-config_${OLTName} config.db_${OLTName} vos.conf_${OLTName} running-config_${OLTName}
@@ -82,15 +86,22 @@ bdcom_olt() {
 for ((i = 0; i < NumProcs; i++)); do
   (
     while read -r OLTName OLTIPAddr OLTModel; do
-      PingResult=$(ping -c 3 -W 1 -A ${OLTIPAddr} | grep transmitted | awk '{ print $6 }')
-      if [[ ${PingResult} != "100%" ]]; then
+      # Ping command
+      PingResult=$(ping -c 3 -W 1 -A ${OLTIPAddr} | grep transmitted | awk '{ print $(NF-4) }')
+      # If the device is available
+      if [[ ${PingResult} =~ ^[0-9]{1,2}\.?[2-9]{0,4}%$ ]]; then
+        # Create a backup directory if it did not exist
         if [[ ! -e ${OLTBackupDir}${OLTName} ]]; then
           mkdir ${OLTBackupDir}${OLTName}
           chown tftp:tftp ${OLTBackupDir}${OLTName}
         fi
-      bdcom_olt
-      # Delete old backups
-      find ${OLTBackupDir}${OLTName} -type f -printf '%T@\t%p\n' | sort | head -n -${BackupCount} | awk '{ print $2 }' | xargs -r rm
+        # Run main function
+        bdcom_olt
+        # Delete old backups
+        find ${OLTBackupDir}${OLTName} -type f -printf '%T@\t%p\n' | sort | head -n -${BackupCount} | awk '{ print $2 }' | xargs -r rm
+      else
+        # Log message about unreachability
+        echo "$(date +%Y-%m-%d_%H-%M-%S) --- ${OLTName} ${OLTIPAddr} is unreachable" >> ${OLTLogDir}unreachable_olts_$(date +%Y-%m-%d).log
       fi
     done < <(awk -v NumProcs=${NumProcs} -v i="$i" 'NR % NumProcs == i { print }' < ${Inventory})
   ) &
@@ -98,4 +109,6 @@ done
 wait
 
 # Delete old logs
-find ${OLTLogDir} -type f -name '*' -mtime +14 -exec rm -rf {} \;
+find ${OLTLogDir} -type f -name '*' -mtime +${LogKeepDays} -delete
+# Archiving logs
+find ${OLTLogDir} -type f -name '*' -mtime +1 -exec gzip -q {} \;
